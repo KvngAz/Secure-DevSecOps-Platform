3b3e63e89f77b0ade31ecca6b2e934eecb871ba4
KvngAz <aladejobiafeez840@gmail.com>
2026-09-24 13:40:29 +0100

    prepare product service for Render

--- services/product-service/app.py
+++ services/product-service/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-
+import os
 app = Flask(__name__)
 
 
@@ -35,4 +35,5 @@ def products():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5003)
+    port = int(os.environ.get("PORT", 5003))
+    app.run(host="0.0.0.0", port=port)
